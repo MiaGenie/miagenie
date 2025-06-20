996e9facc1885fa5891490f9d2a3971826c614fb
David Azevedo <davidazevedo@miagenie.com>
2025-06-20 11:52:17 +0100

    Update assistant model

--- app/Models/Assistant.php
+++ app/Models/Assistant.php
@@ -2,16 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Enums\AssistantType;
 use Inovector\Mixpost\Concerns\Model\HasUuid;
 
 class Assistant extends Model
 {
     use HasUuid;
-    use SoftDeletes;
 
     public $table = 'genie_assistants';
 
